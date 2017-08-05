3b90fd0ea55fedc06ea222e8de5fccf8dc2a3bc9
Felipe@MBP <dataf4l@gmail.com>
2017-08-05 15:25:22 -0500

    Sample data: train data and validation data, sql queries

---
+++ datasets/sale.sql
@@ -0,0 +1,8 @@
+create table sale(
+	DC varchar(255),
+	ID_PRODUCTO varchar(255),
+	COD_CLIENTE varchar(255),
+	FECHA_REAL varchar(255),
+	VALOR_VENTA varchar(255),
+	VALOR_FINANCIADO varchar(255)
+);
