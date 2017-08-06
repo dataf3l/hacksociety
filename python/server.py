e23c310bbe0bca12f1d79ef4a408c905b1f71d70
Felipe@MBP <dataf4l@gmail.com>
2017-08-06 10:29:41 -0500

    add post handler

--- python/server.py
+++ python/server.py
@@ -2,7 +2,18 @@ import cherrypy
 
 
 class HelloWorld(object):
-    def index(self):
+"""
+1.- Nombre
+2.- Tiene productos con este banco ?
+3.- Edad
+4.- Genero ?
+5.- Extranjero ó residente?
+6.- Nivel de educativo? Postgrado, Profesional, Bachiller.
+7.- Nivel de ingresos. menor ó igual a 50000, 100000, 150000, 200000, 250000, ó 300000 
+"""
+
+    def index(self, **json):
+        print(json)
         return """{
          "messages": [
            {"text": "hola, todo funciona"},
