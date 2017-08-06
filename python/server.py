6c9d788357f1458fa177e5ab52a305d302b1b702
Felipe@MBP <dataf4l@gmail.com>
2017-08-06 10:53:45 -0500

    age question

--- python/server.py
+++ python/server.py
@@ -4,6 +4,7 @@ cust = {}
 
 
 class HelloWorld(object):
+
     """
     1.- Nombre
     2.- Tiene productos con este banco ?
@@ -15,15 +16,12 @@ class HelloWorld(object):
     50000, 100000, 150000, 200000, 250000, ó 300000
     """
 
-    def index(self, **json):
+    def handle_name(self):
         global cust
-        if "customer_name" in json:
-            cust["customer_name"] = json["customer_name"]
-
         name = ""
         if "customer_name" in cust:
             name = cust["customer_name"]
-        print(json)
+        print(cust)
         return """{
           "messages": [
             {
@@ -50,6 +48,49 @@ class HelloWorld(object):
           ]
         }
         """
+
+    def handle_age(self):
+        global cust
+        # age = ""
+        # if "age" in cust:
+        #    age = cust["age"]
+        print(cust)
+        return """{
+          "messages": [
+            {
+              "attachment": {
+                "type": "template",
+                "payload": {
+                  "template_type": "button",
+                  "text": "May I know your gender?",
+                  "buttons": [
+                    {
+                      "type": "show_block",
+                      "block_name": "step2_client_is_male",
+                      "title": "Male"
+                    },
+                    {
+                      "type": "show_block",
+                      "block_name": "step2_client_is_female",
+                      "title": "Female"
+                    }
+                  ]
+                }
+              }
+            }
+          ]
+        }
+        """
+
+    def index(self, **json):
+        global cust
+        if "customer_name" in json:
+            cust["customer_name"] = json["customer_name"]
+            return self.handle_name()
+        if "age" in json:
+            cust["age"] = json["age"]
+            return self.handle_age()
+
 
 #        return """{
 #         "messages": [
